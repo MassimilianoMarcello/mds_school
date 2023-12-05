890e20f7928d34d99f65bc002efab4e75a2ae53c
massimiliano <massimarcello@hotmail.it>
2023-12-05 16:07:31 +0100

    precedent commit does not work

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,4 +1,33 @@
-export const MenuItems = [
+// export const MenuItems = [
+//     {
+//       title: 'ABOUT MDS',
+//       path: '/about_mds',
+//       cName: 'dropdown-link'
+//     },
+//     {
+//       title: 'LEADERSHIP',
+//       path: '/leadership',
+//       cName: 'dropdown-link'
+//     },
+//     {
+//       title: 'OUR BOARD',
+//       path: '/our_board',
+//       cName: 'dropdown-link'
+//     },
+//     {
+//       title: 'MDS AT A GLANCE', 
+//       path: '/mds_at_a_glance',
+//       cName: 'dropdown-link'
+//     },
+//     {
+//       title: 'EMPLOYEMENT OPPORTUNITIES', 
+//       path: '/employement_opportunities',
+//       cName: 'dropdown-link'
+//     }
+//   ];
+
+export const MenuItems = {
+  about: [
     {
       title: 'ABOUT MDS',
       path: '/about_mds',
@@ -20,8 +49,124 @@ export const MenuItems = [
       cName: 'dropdown-link'
     },
     {
-      title: 'EMPLOYEMENT OPPORTUNITIES', 
-      path: '/employement_opportunities',
+      title: 'EMPLOYMENT OPPORTUNITIES', 
+      path: '/employment_opportunities',
       cName: 'dropdown-link'
     }
-  ];
+  ],
+  admission: [
+    {
+      title: 'ADMISSION WELCOME',
+      path: '/admission',
+      cName: 'dropdown-link'
+    },  {
+      title: 'ADMISSION PROCESS',
+      path: '/admission',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'TUITION & FINANCIAL AID',
+      path: '/admission',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'INQUIRE',
+      path: '/admission',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'APPLAY TODAY',
+      path: '/admission',
+      cName: 'dropdown-link'
+    },
+    
+  ],
+  inTheClassroom: [
+    {
+      title: 'EARLY CHILDHOOD',
+      path: '/in_the_classroom',
+      cName: 'dropdown-link'
+    },  {
+      title: 'LOWER SCHOOL',
+      path: '/in_the_classroom',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'MIDDLE SCHOOL',
+      path: '/in_the_classroom',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'BRIDGE PROGRAM',
+      path: '/in_the_classroom',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'CURRICULUM GUIDE',
+      path: '/in_the_classroom',
+      cName: 'dropdown-link'
+    },
+    
+  ],
+  parentResources: [
+    {
+      title: 'CALENDAR',
+      path: '/parent_resources',
+      cName: 'dropdown-link'
+    },  {
+      title: 'LUNCH MENU',
+      path: '/parent_resources',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'SHOP MDS',
+      path: '/parent_resources',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'PARENT COUNCIL',
+      path: '/parent_resources',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'AFTER SCHOOL ACTIVITIES',
+      path: '/parent_resources',
+      cName: 'dropdown-link'
+    },
+    
+  ],
+  communityResources: [
+    {
+      title: 'CAMP OROT',
+      path: '/community_resources',
+      cName: 'dropdown-link'
+    },  
+    
+  ],supportingMds: [
+    {
+      title: 'MAKE A GIFT',
+      path: '/support_mds',
+      cName: 'dropdown-link'
+    },  {
+      title: 'WAYS TO GIVE',
+      path: '/support_mds',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'ANNUAL FUND',
+      path: '/support_mds',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'LIMMUD HAYOM',
+      path: '/support_mds',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'PROGRAM SPNSORSHIP',
+      path: '/support_mds',
+      cName: 'dropdown-link'
+    },
+    
+  ]
+};
